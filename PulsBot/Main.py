1d1d4c1d0a891b22907400004690b4931d4f5a23
DanielCurtis1 <dwcurti1@stu.k12.wv.us>
2023-10-18 16:47:49 -0400

    Added tensorflow and a  temporary model.

--- PulsBot/Main.py
+++ PulsBot/Main.py
@@ -1,11 +1,16 @@
-#from kera smth import Tensorflow
+from keras.models import load_model
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import sys
-from PIL import Image
+from PIL import Image, ImageOps
 import re
 import numpy as np
+
+######GET TENSORFLOW TO WORK#######
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
 
 scrapnumber = int(sys.argv[1])
 username = str(sys.argv[2])
@@ -48,8 +53,43 @@ def imagescrap(graphnumber):
     graph.screenshot("NoiseImages/{}.png".format(filename))
 
 def goToNextData():
-    return
+    driver.get("https://pulsars.nanograv.org/psrsearch/surveys/AO327/plots/all")
 
+def teachablemachine(modelname, modellabels, imageslocation):
+    #use Teachable Machine model to see if it is noise or not. First open the file then fead it to TM
+    # Load the model
+    model = load_model("{}".format(modelname), compile=False)
+
+    # Load the labels
+    class_names = open("{}".format(modellabels), "r").readlines()
+
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    image = Image.open("{}.png".format(imageslocation)).convert("RGB")
+
+    # resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+    # turn the image into a numpy array
+    image_array = np.asarray(image)
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # Predicts the model
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+    
+    return class_name, confidence_score
 
 ################################################################## NOISE (DMvT) Analyzing #############################################################
 
@@ -81,40 +121,46 @@ def TrimImage(graphnumber):
 
 #analyzes the DM vs Time graph using Teachable Machine to see what it likely is.
 def TypeOfNoise(fileloc):
-    
-    #use Teachable Machine model to see if it is noise or not. First open the file then fead it to TM
-
-    isNoise = 2
-    confidence = 100
-    return isNoise, confidence
+    classnum = 0
+    class_name, confidence_score = teachablemachine("models/DMvT.h5", "models/DMvTLabels.txt", fileloc)
+    confidence_percent = confidence_score * 100
+    #class_name = 'Pulsar' #placeholder
+    if str(class_name.strip()) == 'Pulsar':
+        classnum = 3
+    elif str(class_name.strip()) == 'RFI':
+        classnum = 2
+    elif str(class_name.strip()) == 'Noise':
+        classnum = 1
+    print(classnum)
+    #confidence_score = 100 #placeholder
+    return classnum, confidence_percent
 
 def PulsType(confidence, fileloc, grnoise):
     writetofile= open(fileloc, "w")
 
+    driver.get("https://pulsars.nanograv.org/psrsearch/search") #temp
+    sleep(1)
+
     if grnoise ==1:
         writetofile.write("[Noise] CONFIDENCE:{}%".format(confidence))
 
-        notes = driver.find_element(By.CLASS_NAME,"dataTables_length") #replace with the notes box
+        notes = driver.find_element(By.ID,"plotid") #replace with the notes box
         notes.send_keys("[Noise] CONFIDENCE:{}% ANALYZED BY PULSBOT https://github.com/DanielCurtis27/PulsBot".format(confidence))
     
-        submit = driver.find_element(By.CLASS_NAME,"dataTables_length") #replace with proper submit button
+        submit = driver.find_element(By.ID,"plotid") #replace with proper submit button
         submit.click()
 
     elif grnoise==2: 
         writetofile.write("[RFI] CONFIDENCE:{}%".format(confidence))
 
-        skip = driver.find_element(By.CLASS_NAME,"dataTables_length") #replace with proper submit button
-        skip.click()
-    elif grnoise==3: 
-        writetofile.write("[Pulsar Candidate] CONFIDENCE:{}%".format(confidence))
-
-        skip = driver.find_element(By.CLASS_NAME,"dataTables_length") #replace with proper submit button
+        skip = driver.find_element(By.ID,"plotid") #replace with proper submit button
         skip.click()
     else: 
         writetofile.write("[Pulsar] CONFIDENCE:{}%".format(confidence))
 
-        skip = driver.find_element(By.CLASS_NAME,"dataTables_length") #replace with proper submit button
+        skip = driver.find_element(By.ID,"plotid") #replace with proper submit button
         skip.click()
+ 
     writetofile.close()
 
 def Noise(graphnumber):
--- PulsBot/ReadMe.txt
+++ PulsBot/ReadMe.txt
@@ -1,4 +1,5 @@
-to start the program, first run the following command (assumes you already have python3 and pip installed):
+to start the program, first run the following commands (assumes you already have python3 and pip installed):
 python3 -m pip install -r install.txt
+python3 -m pip install .\tensorflow-2.14.0-cp311-cp311-win_amd64.whl (if you are not running on windows, find the correct build for your system here: https://pypi.org/project/tensorflow/#files)
 then start the program with the following:
 python3 Main.py 'number of plots to search' 'username' 'password'
